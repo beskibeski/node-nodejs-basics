d78e3245f84a6b96ef0b1d9b1f43aae7315695e4
unknown <benladen1917@inbox.ru>
2024-10-06 19:51:55 +0300

    feat: implement zip and wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
-// n should be received from main thread
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
+import { fileURLToPath } from 'url';
+
+const WORKER_DATA_NUMBER = 10;
+const filePath = fileURLToPath(import.meta.url);
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-};
+    if (isMainThread) {
+        const worker = new Worker(filePath, {workerData: WORKER_DATA_NUMBER});
+        worker.on('message', (msg) => {
+        console.log(msg);
+        })
+    } else {
+        const number = workerData;
+        parentPort.postMessage((nthFibonacci(number)));
+    }
+}
 
 sendResult();
