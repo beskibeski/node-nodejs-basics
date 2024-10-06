613ce14ce7114cc2e59944f39c8d90c796e464dc
unknown <benladen1917@inbox.ru>
2024-10-06 19:07:22 +0300

    feat: implement fs

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { readFile } from 'fs/promises';
+
+const dir = dirname(fileURLToPath(import.meta.url));
+const dirName = 'files';
+const fileName = 'fileToRead.txt';
+const pathToFile = join(dir, dirName, fileName);
+const errorNotExist = 'ENOENT';
+const errorMessage = 'FS operation failed';
+
 const read = async () => {
-    // Write your code here 
+    readFile(pathToFile,  'utf-8')
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            if (err.code === errorNotExist) {
+                throw new Error(errorMessage);
+            }
+        });
 };
 
 await read();
